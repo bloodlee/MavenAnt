50996c1cf366626fd59429f53cb32a24d281d786
Yongqiang Li <jasonleehust@gmail.com>
2015-05-31 18:24:22 -0700

    Initial commit

---
+++ src/main/java/com/innoli/sample/App.java
@@ -0,0 +1,13 @@
+package com.innoli.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(License.VERSION);
+    }
+}
